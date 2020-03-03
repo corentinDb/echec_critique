5422a484051477dedb9ae1eedec5e235ac547c38
Arthur Pourrier <arthur.pourrier@isen.yncrea.fr>
2020-03-03 15:32:16 +0100

    Déplacement du fou

--- server_modules/ClassPieces/Bishop.js
+++ server_modules/ClassPieces/Bishop.js
@@ -7,6 +7,34 @@ class Bishop extends Piece {
     }
 
     //Se déplace en diagonale
+    getMoveList() {
+        for (let i = this.x + 1; i <= 7; i++) {
+            for (let j = this.y + 1; j <= 7; j++) {
+                if ((i <= 7 && j <= 7) /*|| case vide || pièce ennemie*/) {
+                    this.moveList += [i, j];
+                }
+            }
+            for (let j = this.y - 1; j >= 0; j--) {
+                if ((i <= 7 && j >= 0) /*|| case vide || pièce ennemie*/) {
+                    this.moveList += [i, j];
+                }
+            }
+        }
+        for (let i = this.x - 1; i >= 0; i--) {
+            for (let j = this.y - 1; j >= 0; j--) {
+                if ((i >= 0 && j >= 0) /*|| case vide || pièce ennemie*/) {
+                    this.moveList += [i, j];
+                }
+            }
+            for (let j = this.y + 1; j <= 7; j++) {
+                if ((i >= 0 && j <= 7) /*|| case vide || pièce ennemie*/) {
+                    this.moveList += [i, j];
+                }
+            } 
+        }
+
+        return this.moveList;
+    }
 }
 
 module.exports = Bishop;
